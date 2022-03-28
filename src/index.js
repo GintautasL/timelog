fe68ba577babd815726440642e1d3dd4f98153fa
GintautasL <gintautas.lasevicius@gmail.com>
2022-03-28 20:46:40 +0300

    done login

--- src/index.js
+++ src/index.js
@@ -6,17 +6,21 @@ import {
 } from "react-router-dom";
 import App from "./App";
 import {Login} from "./pages/Login";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const theme = createTheme();
 
 const rootElement = document.getElementById("root");
 render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="*" element={<div>404 not found</div>} />
-      
-    </Routes>
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<div>404 not found</div>} />
+        
+      </Routes>
+    </BrowserRouter>
+  </ThemeProvider>,
   rootElement
 );
