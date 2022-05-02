921b258738dddd953d062b9465cc2f72a30e755c
GintautasL <gintautas.lasevicius@gmail.com>
2022-05-02 12:45:08 +0300

    final build

--- src/interceptors.js
+++ src/interceptors.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { logoutRequest } from "./requests"
 
 axios.interceptors.request.use(
   function (config) {
@@ -11,6 +12,24 @@ axios.interceptors.request.use(
   },
   function (error) {
     // Do something with request error
+    return Promise.reject(error)
+  }
+)
+
+axios.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response
+  },
+  function (error) {
+    if (error.response.status == 401) {
+      window.localStorage.clear()
+      window.location.href = "/login"
+    }
+
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
     return Promise.reject(error)
   }
 )
